d24d3754f2760e658a037158aa32c92462c8c6c1
diego.silva <Easy@2021@>
2022-03-02 10:58:48 -0300

    atualiza versao common

--- common/src/Play.Common/Http/HttpClientExtension.cs
+++ common/src/Play.Common/Http/HttpClientExtension.cs
@@ -1,28 +1,56 @@
 namespace Play.Common.Http
 {
     using System;
+    using System.Collections.Generic;
     using System.Net.Mime;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Net.Http.Headers;
     using Settings;
 
+    internal class RequestHeadersMap
+    {
+        private const string Json = "json";
+        private const string Soap = "soap+xml";
+
+        public RequestHeadersMap(string requestHeaderSetting, string requestHeaderClient)
+        {
+            RequestHeaderSetting = requestHeaderSetting;
+            RequestHeaderClient = requestHeaderClient;
+        }
+
+        public readonly string RequestHeaderClient;
+        public readonly string RequestHeaderSetting;
+
+        private static readonly RequestHeadersMap SoapXml = new(Soap, MediaTypeNames.Application.Soap);
+        private static readonly RequestHeadersMap JsonHeader = new(Json, MediaTypeNames.Application.Json);
+
+        private static IEnumerable<RequestHeadersMap?> GetAll()
+        {
+            return new[]
+            {
+                JsonHeader
+            };
+        }
+    }
+
     public static class HttpClientExtension
     {
-        public static IServiceCollection AddCommonClients(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddCommonClients(this IServiceCollection services,
+            IConfiguration configuration)
         {
             var httpClientsSettings = configuration.GetSection(nameof(HttpClientsSettings)).Get<HttpClientsSettings>();
 
             foreach (var clientSetting in httpClientsSettings.Clients)
             {
-                if (string.IsNullOrEmpty(clientSetting.ServiceName) || clientSetting.BaseAddress == null)
+                if (clientSetting.IsValidSetting)
                     continue;
 
                 services.AddHttpClient(clientSetting.ServiceName, client =>
                 {
                     client.BaseAddress = new Uri(clientSetting.BaseAddress);
                     client.DefaultRequestHeaders.Add(HeaderNames.Accept, MediaTypeNames.Application.Json);
-                });
+                }).SetHandlerLifetime(TimeSpan.FromMinutes(clientSetting.HandlerLifetimeInMinutes));
             }
 
             return services;
--- common/src/Play.Common/Http/Settings/HttpClientsSettings.cs
+++ common/src/Play.Common/Http/Settings/HttpClientsSettings.cs
@@ -7,7 +7,23 @@ namespace Play.Common.Http.Settings
 
     public class ClientSetting
     {
+        public ClientSetting()
+        {
+            HandlerLifetimeInMinutes = 2;
+        }
+
         public string ServiceName { get; set; }
         public string? BaseAddress { get; set; }
+
+        public int HandlerLifetimeInMinutes { get; init; }
+
+        public ClientEndpoint RequestEndpoints { get; set; }
+        public bool IsValidSetting => string.IsNullOrEmpty(ServiceName) || BaseAddress == null;
+    }
+
+    public class ClientEndpoint
+    {
+        public string Path { get; set; }
+        public string[] AcceptedHeaders { get; set; }
     }
 }
